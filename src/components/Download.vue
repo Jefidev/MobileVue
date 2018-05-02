b5be4e372a1dc7066bc71468e2fa2539a7c02492
Oussama Essafi <oussama-essafi@hotmail.com>
2018-05-02 19:41:56 +0200

    Updated download page

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -4,9 +4,9 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="intro">
-                        <h2>Highlight</h2>
-                        <p>Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. Aliqua sed justo ligula.</p>
-                        <a class="btn btn-primary" role="button" href="#">Action Button</a></div>
+                        <h2>The KYT game</h2>
+                        <p>Discover the town in a fun way, download the app and start the adventure today !</p>
+                        <a class="btn btn-success" role="button" href="#">Download</a></div>
                 </div>
                 <div class="col-sm-4">
                     <div class="d-none d-md-block iphone-mockup"><img src="static/img/iphone.svg" class="device">
