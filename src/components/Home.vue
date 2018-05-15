08cce98bac7bbf4931228d1bae345ae46c844b30
Valentin Eggermont <valentin.eggermont@student.unamur.be>
2018-05-15 12:08:46 +0200

    tweets removed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,68 +44,8 @@
               </div>
             </div>
             <br/><br/>
-            <div class="row">
-              <div class="col-md-12 card-deck">
-                <div class="col-md-4">
-                 <div class="card text-white bg-info mb-3" style="">
-                    <div class="card-header">Tweet 1</div>
-                    <div class="card-body">
-                      <h5 class="card-title"></h5>
-                      <p class="card-text">Hi, my name is Jeff ! #ThatsAMovieReference</p>
-                    </div>
-                  </div>
-                  </div>
-                  <div class="col-md-4">
-                  <div class="card text-white bg-info mb-3" style="">
-                    <div class="card-header">Tweet 2</div>
-                    <div class="card-body">
-                      <h5 class="card-title"></h5>
-                      <p class="card-text">Et là vous avez des tweets... #çaBosseProjetMobile</p>
-                    </div>
-                  </div>
-                  </div>
-                  <div class="col-md-4">
-                  <div class="card text-white bg-info mb-3" style="">
-                    <div class="card-header">Tweet 3</div>
-                    <div class="card-body">
-                      <h5 class="card-title"></h5>
-                      <p class="card-text">Hellooooo ! #JeSuisNouveauIci</p>
-                    </div>
-                  </div>
-                </div>
-                </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12 card-deck">
-                <div class="col-md-4">
-                 <div class="card text-white bg-info mb-3" style="">
-                    <div class="card-header">Tweet 4</div>
-                    <div class="card-body">
-                      <h5 class="card-title"></h5>
-                      <p class="card-text">A mon avis ça va être juste juste #RendezVousAout2018</p>
-                    </div>
-                  </div>
-                  </div>
-                  <div class="col-md-4">
-                  <div class="card text-white bg-info mb-3" style="">
-                    <div class="card-header">Tweet 5</div>
-                    <div class="card-body">
-                      <h5 class="card-title"></h5>
-                      <p class="card-text">-50% sur toutes les boissons au monde à l'envers ! #GoGoGo</p>
-                    </div>
-                  </div>
-                  </div>
-                  <div class="col-md-4">
-                  <div class="card text-white bg-info mb-3" style="">
-                    <div class="card-header">Tweet 6</div>
-                    <div class="card-body">
-                      <h5 class="card-title"></h5>
-                      <p class="card-text">C'est quoi ce tag "Projacquet" sur la facade de la fac info ? #OohçaMaLairChelou</p>
-                    </div>
-                  </div>
-                </div>
-                </div>
-            </div>
+
+
          </div>
     </div>
 
